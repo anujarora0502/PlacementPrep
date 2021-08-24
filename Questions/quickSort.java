4a0f1c964b65b330b10f0298bd09ab8b8999e834
anujarora0502 <anujarora0502@gmail.com>
2021-08-25 03:38:13 +0530

    quick sort algo

---
+++ Questions/quickSort.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class quickSort {
+    public static void main(String[] args) {
+       int[] arr = {8,7,6,1,0,9,2};
+
+       quickSortFn(arr, 0, arr.length - 1);
+
+       print1DArr(arr);
+    }
+
+    public static void quickSortFn(int[] arr, int start, int end) {
+        if(start<end){ 
+        int partitioningIndex = partitioning(arr, start, end);
+
+        quickSortFn(arr, start, partitioningIndex - 1);
+        quickSortFn(arr, partitioningIndex + 1, end);
+        }
+    }
+
+    public static int partitioning(int[] arr, int start, int end) {
+        int pivot = end;
+
+        int pointer = start;
+
+        for(int i = start;i<=end;i++){
+            if(arr[i]<arr[pivot]){
+                swap(i,pointer,arr);
+                pointer++;
+            }
+        }
+
+        swap(pointer, pivot, arr);
+
+        return pointer;
+    }
+
+    public static void print1DArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int a, int b, int[] arr) {
+
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
+}
